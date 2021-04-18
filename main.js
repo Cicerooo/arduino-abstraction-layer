d38b3a1005349c4fcfab4f9fd03d2b04fd3fdba6
Vlad Preda <vladpredahuh@gmail.com>
2021-04-18 20:39:05 +0300

    register serial data as json

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
+// Serial stuff
 const SerialPort = require('serialport');
-const Readline = require('@serialport/parser-readline');const port = new SerialPort('COM4', { baudRate: 115200 });
+const Readline = require('@serialport/parser-readline');
+const port = new SerialPort('COM4', { baudRate: 115200 });
 const parser = port.pipe(new Readline({ delimiter: '\n' }));// Read the port data
+// Input robot
+const robot = require("robotjs");
+
+const dataToSkip = 100;
+let currentData = 0;
 let inputData = {
   "c1":1,
   "x":0,
@@ -9,25 +16,53 @@ let inputData = {
 };
 let assignedValue = "";
 
+//List all serial ports available
+SerialPort.list().then(function (data) {
+  console.log(data);  
+});
+
 port.on("open", () => {
   console.warn('serial port open');
-});
-parser.on('data', data =>{
-  parseNextInput(data);
-  if(data.includes("DATA_START")){
-      //port.close();
-      console.clear();
-  }
-  console.log('input:', data);
 });
 port.on('close', ()=>{
   console.log(JSON.stringify(inputData));
 })
+
+parser.on('data', data =>{
+  if(data.includes("}")){
+    parseNextInput(data);
+  }  
+  showDataOnScreen();
+});
+
+function showDataOnScreen(){
+  if(currentData==dataToSkip){
+    console.clear();
+    console.log(inputData)
+    currentData = 0;
+  }
+  else currentData++;
+}
+
 function parseNextInput(input){
-  if(parseInt(input)!=NaN){
-    inputData[assignedValue] = parseInt(input);
+  try {
+    inputData = JSON.parse(input);
   }
-  else{
-    assignedValue = input;
+  catch(e){
+    console.log(e) 
+    console.log(input)
   }
+  finally{
+    processInput();
+  }
+}
+function processInput(){
+  if(inputData.y>700)
+    robot.keyTap("w")
+  if(inputData.x>700)
+    robot.keyTap("a")
+  if(inputData.y<300)
+    robot.keyTap("s")
+  if(inputData.x<300)
+    robot.keyTap("d")
 }
